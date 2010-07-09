eeabb0e5e79208d123938925abc87eb231fdc760
Danny Wilson <danny@onlinetouch.nl>
2010-07-09 16:26:07 +0200

    Added 2-clause BSD license, in the style of the haXe standard library license.
    Added small doc-comments here and there.

--- src/primevc/core/dispatcher/Wire.hx
+++ src/primevc/core/dispatcher/Wire.hx
@@ -1,8 +1,44 @@
+/*
+ * Copyright (c) 2010, The PrimeVC Project Contributors
+ * All rights reserved.
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *   - Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   - Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in the
+ *     documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE PRIMEVC PROJECT CONTRIBUTORS "AS IS" AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE PRIMVC PROJECT CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
+ * DAMAGE.
+ *
+ *
+ * Authors:
+ *  Danny Wilson	<danny @ onlinetouch.nl>
+ */
 package primevc.core.dispatcher;
  import primevc.core.IDisposable;
  import primevc.core.ListNode;
   using primevc.utils.BitUtil;
 
+/**
+ * A Wire is the connection between a Signal0-4 dispatcher, and a handler object+function.
+ * 
+ * This allows to quickly (temporarily) disconnect (disable) the handler from the signal dispatcher.
+ * 
+ * Implementation detail: Wires are added to a bounded freelist (max 256 free objects) to reduce garbage collector pressure.
+ * This means you should never reuse a Wire after calling dispose() and/or after unbinding the handler from the signal (which returned this Wire).
+ */
 class Wire <FunctionSignature> extends ListNode<Wire<FunctionSignature>>, implements IDisposable
 {
 	/** Wire.flags bit which tells if the Wire is isEnabled(). */
