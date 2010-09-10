4fa2b83cb1b5fcc48516c638a1a3de08a8c28f2c
Danny Wilson <danny@decube.net>
2010-09-10 17:44:42 +0200

    Extra isNaN tests.

--- tests/cases/IsNan.hx
+++ tests/cases/IsNan.hx
@@ -9,10 +9,17 @@ class IsNan
 		var nan:Float = Math.NaN;
 		var nullf:Float = #if !flash9 null #else nan #end;
 		
+		var one = 1.17264870091264061;
+		var two = one * 200000.010012031239701793097;
+		var three = two / 300000.123987163916986896;
+		
 	#if debug	
 		Assert.that(float.isSet());
 		Assert.that(nan.notSet());
 		Assert.that(nullf.notSet());
+		Assert.that(one.isSet());
+		Assert.that(two.isSet());
+		Assert.that(three.isSet());
 		trace("IsNan tests passed!");
 	#else
 		trace("Recompile in DEBUG mode!");
